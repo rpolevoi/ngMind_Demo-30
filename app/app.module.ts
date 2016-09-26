25e3d9cce0084d158d07288438ba05a8dd4b3c84
Robert Polevoi <robert.polevoi@gmail.com>
2016-09-26 08:34:29 -0700

    Create app.module.ts

---
+++ app/app.module.ts
@@ -0,0 +1,27 @@
+import { HttpModule } from '@angular/http';
+import { InMemoryWebApiModule }     from 'angular2-in-memory-web-api';
+import { MemberData }                 from './member-data';
+  
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+
+import { AppComponent }   from './app.component';
+import { routing }  from './app.routing';
+
+import { RouteAComponent } from './route_a/route_a.component';
+import { RouteBComponent } from './route_b/route_b.component';
+import { ChildComponent } from './route_a/child/child.component';
+
+import { MemberService } from './member.service';
+
+
+@NgModule({
+  imports: [BrowserModule, HttpModule, routing, InMemoryWebApiModule.forRoot(MemberData)],
+  declarations: [ AppComponent,
+                RouteAComponent,
+                RouteBComponent,
+                ChildComponent ],
+  providers: [ MemberService  ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
